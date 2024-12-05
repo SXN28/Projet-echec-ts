28913d27359f690b5630c70201328f516d72f247
Samalou <137214216+Samalou@users.noreply.github.com>
2024-12-05 12:25:00 +0100

    début journéé

--- frontend/src/components/ChessBoard.vue
+++ frontend/src/components/ChessBoard.vue
@@ -1,21 +1,144 @@
+<script setup lang="ts">
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
+import kingWhite from "@/assets/chess-pieces/king_w.png";
+import queenWhite from "@/assets/chess-pieces/queen_w.png";
+import rookWhite from "@/assets/chess-pieces/rook_w.png";
+import bishopWhite from "@/assets/chess-pieces/bishop_w.png";
+import knightWhite from "@/assets/chess-pieces/knight_w.png";
+import pawnWhite from "@/assets/chess-pieces/pawn_w.png";
+import kingBlack from "@/assets/chess-pieces/king_b.png";
+import queenBlack from "@/assets/chess-pieces/queen_b.png";
+import rookBlack from "@/assets/chess-pieces/rook_b.png";
+import bishopBlack from "@/assets/chess-pieces/bishop_b.png";
+import knightBlack from "@/assets/chess-pieces/knight_b.png";
+import pawnBlack from "@/assets/chess-pieces/pawn_b.png";
+
+let board = ref([]);
+let currentTurn = ref("white");
+let selectedPiece = ref<{ row: number; col: number } | null>(null);
+let gameId = ref(15);
+let playerColor = ref("black");
+
+type PieceType = "K" | "Q" | "R" | "B" | "N" | "P";
+
+type Cell = {
+  color: "white" | "black";
+  piece: PieceType;
+};
+
+onMounted(() => {
+  loadBoard();
+  getCurrentTurn();
+});
+
+function loadBoard() {
+  axios
+      .get(`http://localhost:8000/games/${gameId.value}`)
+      .then((response) => {
+        board.value = JSON.parse(response.data.board);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la récupération des données :", error);
+      });
+}
+
+function getPieceImage(cell: { color: string; piece: string }) : any {
+  const pieceImages = {
+    white: {
+      K: kingWhite,
+      Q: queenWhite,
+      R: rookWhite,
+      B: bishopWhite,
+      N: knightWhite,
+      P: pawnWhite,
+    },
+    black: {
+      K: kingBlack,
+      Q: queenBlack,
+      R: rookBlack,
+      B: bishopBlack,
+      N: knightBlack,
+      P: pawnBlack,
+    },
+  };
+
+  return pieceImages[cell.color][cell.piece];
+}
+function onDragStart(rowIndex: number, colIndex: number) {
+  selectedPiece.value = { row: rowIndex, col: colIndex };
+}
+
+function getCurrentTurn() {
+  axios
+      .get(`http://localhost:8000/games/${gameId.value}`)
+      .then((response) => {
+        currentTurn.value = response.data.turn;
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la récupération du tour actuel :", error);
+      });
+}
+
+function allowDrop(event: Event) {
+  event.preventDefault();
+}
+
+function onDrop(targetRow: number, targetCol: number) {
+  if (!selectedPiece.value) return;
+
+  const { row, col } = selectedPiece.value;
+
+  axios
+      .post(`http://localhost:8000/moves`, {
+        gameId: gameId.value,
+        fromRow: row,
+        fromCol: col,
+        toRow: targetRow,
+        toCol: targetCol,
+        playerColor: playerColor.value,
+      })
+      .then(() => {
+        loadBoard();
+        selectedPiece.value = null;
+      })
+      .catch((error) => {
+        console.error("Erreur lors du déplacement :", error);
+        selectedPiece.value = null;
+      });
+}
+</script>
+
 <template>
   <div class="chessboard">
+
+    <div class="controls">
+      <label for="gameIdInput">ID de la partie :</label>
+      <input
+          type="number"
+          id="gameIdInput"
+          v-model.number="gameId"
+          @change="loadBoard"
+      />
+      <p>C'est au tour des {{ currentTurn }}</p>
+    </div>
+
+
     <div
         v-for="(row, rowIndex) in board"
         :key="rowIndex"
-        class="row"
-    >
+        class="row">
       <div
           v-for="(cell, colIndex) in row"
           :key="colIndex"
           class="cell"
           :class="{
-          black: (rowIndex + colIndex) % 2 === 0,
-          white: (rowIndex + colIndex) % 2 !== 0,
-        }"
+            black: (rowIndex + colIndex) % 2 === 0,
+            white: (rowIndex + colIndex) % 2 !== 0,
+          }"
           @dragover.prevent="allowDrop"
-          @drop="onDrop(rowIndex, colIndex)"
-      >
+          @drop="onDrop(rowIndex, colIndex)">
         <img
             v-if="cell"
             :src="getPieceImage(cell)"
@@ -29,102 +152,6 @@
   </div>
 </template>
 
-<script>
-import axios from "axios";
-
-// Import explicite des images des pièces d'échecs
-import kingWhite from "@/assets/chess-pieces/king_w.png";
-import queenWhite from "@/assets/chess-pieces/queen_w.png";
-import rookWhite from "@/assets/chess-pieces/rook_w.png";
-import bishopWhite from "@/assets/chess-pieces/bishop_w.png";
-import knightWhite from "@/assets/chess-pieces/knight_w.png";
-import pawnWhite from "@/assets/chess-pieces/pawn_w.png";
-
-import kingBlack from "@/assets/chess-pieces/king_b.png";
-import queenBlack from "@/assets/chess-pieces/queen_b.png";
-import rookBlack from "@/assets/chess-pieces/rook_b.png";
-import bishopBlack from "@/assets/chess-pieces/bishop_b.png";
-import knightBlack from "@/assets/chess-pieces/knight_b.png";
-import pawnBlack from "@/assets/chess-pieces/pawn_b.png";
-
-export default {
-  name: "ChessBoard",
-  data() {
-    return {
-      board: [],
-      selectedPiece: null, // Pour stocker la pièce sélectionnée
-      gameId: 9, // ID de la partie
-      playerColor: "black", // Couleur du joueur
-    };
-  },
-  mounted() {
-    this.loadBoard();
-  },
-  methods: {
-    loadBoard() {
-      axios
-          .get(`http://localhost:8000/games/${this.gameId}`)
-          .then((response) => {
-            this.board = JSON.parse(response.data.board);
-          })
-          .catch((error) => {
-            console.error("Erreur lors de la récupération des données :", error);
-          });
-    },
-    getPieceImage(cell) {
-      const pieceImages = {
-        white: {
-          K: kingWhite,
-          Q: queenWhite,
-          R: rookWhite,
-          B: bishopWhite,
-          N: knightWhite,
-          P: pawnWhite,
-        },
-        black: {
-          K: kingBlack,
-          Q: queenBlack,
-          R: rookBlack,
-          B: bishopBlack,
-          N: knightBlack,
-          P: pawnBlack,
-        },
-      };
-
-      return pieceImages[cell.color][cell.piece];
-    },
-    onDragStart(rowIndex, colIndex) {
-      this.selectedPiece = {row: rowIndex, col: colIndex};
-    },
-    allowDrop(event) {
-      event.preventDefault();
-    },
-    onDrop(targetRow, targetCol) {
-      if (!this.selectedPiece) return;
-
-      const {row, col} = this.selectedPiece;
-
-      axios
-          .post(`http://localhost:8000/moves`, {
-            gameId: this.gameId,
-            fromRow: row,
-            fromCol: col,
-            toRow: targetRow,
-            toCol: targetCol,
-            playerColor: this.playerColor,
-          })
-          .then(() => {
-            this.loadBoard();
-            this.selectedPiece = null;
-          })
-          .catch((error) => {
-            console.error("Erreur lors du déplacement :", error);
-            this.selectedPiece = null;
-          });
-    },
-  },
-};
-</script>
 
 <style scoped>
 .chessboard {
@@ -132,6 +159,13 @@ export default {
   display: flex;
   align-items: center;
   flex-direction: column;
+}
+
+.controls {
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
 }
 
 .row {
